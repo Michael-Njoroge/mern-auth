2095144da115a1b27c455b94c78124863a42e878
Michael-Njoroge <mikethecoder12@gmail.com>
2023-10-22 19:04:38 +0300

    first commit

---
+++ backend/controllers/userController.js
@@ -0,0 +1,9 @@
+//@desc   Auth user/set token
+//route     POST /api/users/auth
+//@access   Public
+
+const authUser = (req, res) => {
+    res.status(200).json({message:'Auth User'})
+}
+
+export {authUser};
