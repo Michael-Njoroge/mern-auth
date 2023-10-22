0e0f1f0dc20d0b3a525e6ad5e79e19b9db32f7e7
Michael-Njoroge <mikethecoder12@gmail.com>
2023-10-22 19:47:08 +0300

    create error handler and the user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,13 @@
+import asyncHandler from 'express-async-handler'
 //@desc   Auth user/set token
 //route     POST /api/users/auth
 //@access   Public
 
-const authUser = (req, res) => {
-    res.status(200).json({message:'Auth User'})
-}
+const authUser = asyncHandler(async(req, res) => {
+    res.status(401);
+    throw new Error('Something went wrong');
 
-export {authUser};
+    res.status(200).json({message:'Auth User'});
+});
+
+export { authUser };
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
-import  express  from "express";
+import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
-import userRoutes from './routes/userRoutes.js'
+import userRoutes from './routes/userRoutes.js';
+import { notFound,errorHandler } from './middleware/errorMiddleware.js';
 
 const port = process.env.PORT || 5000;
 
@@ -10,5 +11,8 @@ const app = express();
 app.use('/api/users', userRoutes);
 
 app.get('/', (req,res) => res.send('Server is ready'));
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
