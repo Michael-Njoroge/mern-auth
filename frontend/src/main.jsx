2b6fa8cd8375059142c5e4fcd34d6c196cfa2163
Michael-Njoroge <mikethecoder12@gmail.com>
2023-10-26 13:24:37 +0300

    creating user slice using redux

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,8 @@ import './index.css'
 import HomeScreen from './screens/HomeScreen.jsx'
 import LoginScreen from './screens/LoginScreen.jsx'
 import RegisterScreen from './screens/RegisterScreen.jsx'
+import store from './store.js'
+import { Provider } from 'react-redux'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -20,7 +22,9 @@ const router = createBrowserRouter(
 )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
   <React.StrictMode>
    <RouterProvider router={router}/>
-   </React.StrictMode>,
+   </React.StrictMode>
+   </Provider>
 )
