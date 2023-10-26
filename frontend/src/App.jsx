c2001b26fed0ea4437937a9b743d1c299d7bbaaa
Michael-Njoroge <mikethecoder12@gmail.com>
2023-10-26 15:21:17 +0300

    install and use react-toast for error responses

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header"
 import { Container } from "react-bootstrap";
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
  
 const App = () =>  {
   return (
     <>
     <Header/>
+    <ToastContainer/>
     <Container className="my-2">
     <Outlet/>
     </Container>
--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -5,6 +5,7 @@ import FormContainer from '../components/FormContainer';
 import { useDispatch,useSelector } from "react-redux";
 import { useLoginMutation } from "../slices/userApiSlice";
 import {setCredentials} from '../slices/authSlice'
+import { toast } from "react-toastify";
 
 const LoginScreen = () => {
 
@@ -31,7 +32,7 @@ const LoginScreen = () => {
             dispatch(setCredentials({...res}))
             navigate('/')
         } catch (err) {
-            console.log(err?.data?.message || err.error)
+            toast.error(err?.data?.message || err.error)
         }
      }
 
