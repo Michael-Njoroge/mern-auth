c296d216da3dab6ca149aa12f8f3d9d4300484ca
Michael-Njoroge <mikethecoder12@gmail.com>
2023-10-27 14:20:42 +0300

    server.js editting

--- backend/server.js
+++ backend/server.js
@@ -20,20 +20,18 @@ app.use(cookieParser());
 
 app.use('/api/users', userRoutes);
 
-     const __dirname = path.resolve();
-      
-    app.use(express.static(path.join(__dirname,'/frontend/dist')));
+if(process.env.NODE_ENV === 'production'){
+    const __dirname = path.resolve();
+    app.use(express.static(path.join(__dirname,'frontend/dist')));
 
-    app.get('*', (req,res) => {
-        res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
-    })
- 
-    app.get('/', (req,res) => res.send('Server is ready'));
- 
+    app.get('*', (req,res) =>res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html')));
+}else {
+app.get('/', (req,res) => res.send('Server is ready'));
+}
 
 
 
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.listen(port, () => console.log(`Server started on port ${port}`));
