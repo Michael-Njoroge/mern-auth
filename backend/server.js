a3698ce8ba0ac8e9a52c271dbad52a90e7080b67
Michael-Njoroge <mikethecoder12@gmail.com>
2023-10-27 13:00:24 +0300

    minor changes on deployment

--- backend/server.js
+++ backend/server.js
@@ -20,14 +20,16 @@ app.use(cookieParser());
 
 app.use('/api/users', userRoutes);
 
-if(process.env.NODE_ENV === 'production'){
-    const __dirname = path.resolve();
-    app.use(express.static(path.join(__dirname,'frontend/dist')));
+     const __dirname = path.resolve();
+      
+    app.use(express.static(path.join(__dirname,'/frontend/dist')));
 
-    app.get('*', (req,res) =>res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html')));
-}else {
+    app.get('*', (req,res) => {
+        res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+    })
+ 
     app.get('/', (req,res) => res.send('Server is ready'));
-}
+ 
 
 
 
